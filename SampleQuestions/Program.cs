515bcc9ddad6491d51d0012b24725920afd51be1
Damian Brady <dev@damianbrady.com.au>
2015-08-05 20:34:26 +1000

    Empty Console App

---
+++ SampleQuestions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started. Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
